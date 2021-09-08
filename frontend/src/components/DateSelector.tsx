4e282c6638181d37161eda846efc232b968178c1
Lachlan Ting <18lt01@gmail.com>
2021-09-08 23:30:10 +1000

    disable feature to choose date before current

--- frontend/src/components/DateSelector.tsx
+++ frontend/src/components/DateSelector.tsx
@@ -4,15 +4,16 @@ import DateFnsUtils from '@date-io/date-fns';
 
 function DateSelector() {
     const [date, changeDate] = useState<Date | null>(new Date());
-    
+  
     return (
       <MuiPickersUtilsProvider utils={DateFnsUtils}>
-          <DatePicker
-          autoOk
-          variant="static"
-          openTo="date"
-          value={date}
-          onChange={(date) => changeDate(date)}
+        <DatePicker
+        autoOk
+        disablePast
+        variant="static"
+        openTo="date"
+        value={date}
+        onChange={(date) => changeDate(date)}
         />
       </MuiPickersUtilsProvider>
 
